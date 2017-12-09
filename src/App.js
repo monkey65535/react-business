b82aca672359ec178e654674554dd5294cf46dc2
dilidili <466311467@qq.com>
2017-12-09 17:22:28 +0800

    reset.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import './App.scss';
+import {Button} from 'antd';
 
 class App extends Component {
   render() {
@@ -11,6 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Button>这是一个BUTTON</Button>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './reset.css';
+import './index.scss';
 import registerServiceWorker from './registerServiceWorker';
-
+const App = () => (<h2>页面初始化完成</h2>)
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
