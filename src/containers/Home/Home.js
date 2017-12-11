0ee748e96912463cfb9882c521fc92606115a07d
dilidili <466311467@qq.com>
2017-12-11 17:45:31 +0800

    util end

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
+import Axios from 'axios';
+import {message} from 'antd';
 class Home extends Component {
     render() {
+        Axios.get('./product/list.do?keyword=1').then(res=>{
+            console.log('res',res);
+        },rej=>{
+            message.destroy();
+        })
         return (
             <div>
                 首页
+                <i className="fa fa-user"></i>
             </div>
         );
     }
--- src/util/util.js
+++ src/util/util.js
@@ -1,2 +1,46 @@
-// 表单验证
-// 链接跳转
+import {message} from 'antd';
+
+const conf = {
+    serverHost: ''
+}
+// 获取服务端地址
+export function getServerUrl(path) {
+    return conf.serverHost + path;
+}
+// 成功提示
+export function successTips(msg) {
+    message.success(msg || '操作成功');
+}
+// 错误提示
+export function errorTips(msg) {
+    message.error(msg || '哪里不对了哟~');
+}
+
+// 表单验证  支持非空判断，手机号码，邮箱验证
+export function validate(value,type){
+    value = value.trim();
+    // 非空验证
+    if(type === 'require'){
+        return !!value;
+    }
+
+    // 验证手机号
+    if(value === 'phone'){
+        return /^1\d{10}$/.test(value);
+    }
+    //邮箱验证
+    if(value === 'email'){
+        return /^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/.test(value)
+    }
+
+}
+//链接跳转
+export function goLink({history, params, to}) {
+    if (history && to) {
+        if (params) {
+            history.push(`${to}/${params}`);
+        } else {
+            history.push(to);
+        }
+    }
+}
